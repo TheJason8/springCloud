40f4c6855ccde471d6e8bc888cd7de33ae2c3fcf
jason <xieyue.410686417@qq.com>
2018-12-29 10:12:30 +0800

    springCloud整合Eureka、ribbon、feign、hystrix 整合Redis

--- web-produce/src/main/java/com/example/service/impl/BookServiceImpl.java
+++ web-produce/src/main/java/com/example/service/impl/BookServiceImpl.java
@@ -5,12 +5,11 @@ import com.example.dao.BookDao;
 import com.example.entity.Book;
 import com.example.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
-
+import java.util.ArrayList;
 import java.util.List;
+
 
 
 @Service
@@ -19,13 +18,36 @@ public class BookServiceImpl implements BookService {
     @Autowired
     private BookDao dao;
 
+    @Autowired
+    private RedisTemplate redisTemplate;//变量名不能为template 否则自动注入报错
+
     @Override
     public List<Book> findAll() {
-        return dao.findAll();
+        List<Book> bookList=new ArrayList<>();
+        bookList= redisTemplate.boundHashOps("bookList").values();
+        if (bookList==null || bookList.size()==0){
+            bookList=dao.findAll();
+            for (Book book : bookList) {
+                redisTemplate.boundHashOps("bookList").put(book.getBookName(),book);
+            }
+        }else{
+            System.out.println("从redis缓存中获取");
+        }
+        return bookList;
     }
 
     @Override
     public Book findByName(String name) {
-        return dao.findByName(name);
+        Book book=new Book();
+        book = (Book) redisTemplate.boundHashOps("bookList").get(name);
+        if(book==null){
+            book=dao.findByName(name);
+            if(book!=null){
+                redisTemplate.boundHashOps("bookList").put(name,book);
+            }
+        }else{
+            System.out.println("从缓存中获取书本信息");
+        }
+        return book;
     }
 }
